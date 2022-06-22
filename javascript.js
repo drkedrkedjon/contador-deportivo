77879a658b997b98a0abf0dcdb402af3fcc4216d
Sasa Savic Perusina <sasa@bubulazy.com>
2022-06-22 14:09:55 +0100

    HTML y CSS terminado

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log('sasa')
+const displayHome = document.querySelector('#display-home')
+const displayGuest = document.querySelector('#display-guest')
+
+const homePuntos1 = document.querySelector('#home-puntos-1')
+const homePuntos2 = document.querySelector('#home-puntos-2')
+const homePuntos3 = document.querySelector('#home-puntos-3')
+
+const guestPuntos1 = document.querySelector('#guest-puntos-1')
+const guestPuntos2 = document.querySelector('#guest-puntos-2')
+const guestPuntos3 = document.querySelector('#guest-puntos-3')
+
+const btnStart = document.querySelector('#btn-start')
