e63c8c206939b45e781a84c2794c3c8d80caaded
Sasa Savic Perusina <sasa@bubulazy.com>
2022-06-23 12:27:59 +0100

    JS de puntuacion terminada, falta implementar el tiempo

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+const nombreHome = document.querySelector('#nombre-home')
+const nombreGuest = document.querySelector('#nombre-guest')
+
 const displayHome = document.querySelector('#display-home')
 const displayGuest = document.querySelector('#display-guest')
 
@@ -10,3 +13,76 @@ const guestPuntos2 = document.querySelector('#guest-puntos-2')
 const guestPuntos3 = document.querySelector('#guest-puntos-3')
 
 const btnStart = document.querySelector('#btn-start')
+
+let puntosHome = 0
+let puntosGuest = 0
+
+function renderHomeDisplay(num) {
+  displayHome.textContent = num
+  colorGanador()
+}
+function renderGuestDisplay(num) {
+  displayGuest.textContent = num
+  colorGanador()
+}
+
+homePuntos1.addEventListener('click', function() {
+  puntosHome++
+  renderHomeDisplay(puntosHome)
+})
+homePuntos2.addEventListener('click', function() {
+  puntosHome += 2
+  renderHomeDisplay(puntosHome)
+})
+homePuntos3.addEventListener('click', function() {
+  puntosHome += 3
+  renderHomeDisplay(puntosHome)
+})
+
+guestPuntos1.addEventListener('click', function() {
+  puntosGuest++
+  renderGuestDisplay(puntosGuest)
+})
+guestPuntos2.addEventListener('click', function() {
+  puntosGuest += 2
+  renderGuestDisplay(puntosGuest)
+})
+guestPuntos3.addEventListener('click', function() {
+  puntosGuest += 3
+  renderGuestDisplay(puntosGuest)
+})
+
+function colorGanador() {
+  if (puntosHome > puntosGuest) {
+    nombreHome.classList.remove("rojo")
+    nombreHome.classList.remove("amarillo")
+    nombreHome.classList.add("verde")
+
+    nombreGuest.classList.remove("verde")
+    nombreGuest.classList.remove("amarillo")
+    nombreGuest.classList.add("rojo")
+  } else if (puntosHome < puntosGuest) {
+    nombreHome.classList.remove("verde")
+    nombreHome.classList.remove("amarillo")
+    nombreHome.classList.add("rojo")
+
+    nombreGuest.classList.remove("rojo")
+    nombreGuest.classList.remove("amarillo")
+    nombreGuest.classList.add("verde")
+  } else {
+    nombreHome.classList.remove("verde")
+    nombreHome.classList.remove("rojo")
+    nombreHome.classList.add("amarillo")
+
+    nombreGuest.classList.remove("rojo")
+    nombreGuest.classList.remove("verde")
+    nombreGuest.classList.add("amarillo")
+  }
+}
+
+btnStart.addEventListener('click', function() {
+  puntosHome = 0
+  puntosGuest = 0
+  renderHomeDisplay(puntosHome)
+  renderGuestDisplay(puntosGuest)
+})
